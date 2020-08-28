c42ef618c01c4eaadd56216f4459d5c8affdee6e
Max Monis <mmonis77@gmail.com>
2020-08-28 17:10:35 -0400

    improve responsiveness of home

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,23 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
+
+const Text = styled.div`
+  padding-top: 3rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    column-gap: 2rem;
+  }
+  p {
+    line-height: 2;
+  }
+`
 
 const Home = () => {
   const info = useStaticQuery(graphql`
@@ -17,10 +35,23 @@ const Home = () => {
       }
     }
   `)
-  console.log(info)
+  const { title, content, image } = info.allDatoCmsPage.nodes[0]
   return (
     <>
-      <h2>Title of Page</h2>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 3rem;
+          padding: 0 2rem;
+        `}
+      >
+        {title}
+      </h2>
+      <Text>
+        <p>{content}</p>
+        <Image fluid={image.fluid} />
+      </Text>
     </>
   )
 }
